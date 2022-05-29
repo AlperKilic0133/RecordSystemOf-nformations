32a47294e9a363e0e49759f261b89902ec82a545
AlperKilic0133 <96659011+AlperKilic0133@users.noreply.github.com>
2022-05-29 17:01:45 +0300

    first project

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Productdal _productdal = new Productdal();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            urunyukle();
+        }
+
+        private void urunyukle()
+        {
+             dtgProduct.ItemsSource=_productdal._TakeProduct();
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            _productdal.Add(new Product { Carsname = txtProductName.Text,
+                CarsPrice = Convert.ToDecimal(txtProductPrice.Text),
+                CustomerName=txtCustomerName.Text
+               
+            }) ;
+                {
+                 
+            }
+            urunyukle();
+        }
+
+        private void dtgProduct_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Product product = new Product();
+            product = (Product)dtgProduct.SelectedItem;
+            grd1.DataContext = product;
+
+        }
+
+        private void btn_clean_Click(object sender, RoutedEventArgs e)
+        {
+            txtId.Text = string.Empty;
+            txtProductName.Text = string.Empty;
+            txtCustomerName.Text = string.Empty;
+            txtProductPrice.Text = string.Empty;
+        }
+
+        private void btn_update_Click(object sender, RoutedEventArgs e)
+        {
+            _productdal.Update(new Product {
+                CustomerId=Convert.ToInt32(txtId.Text),
+                Carsname = txtProductName.Text,
+                CarsPrice = Convert.ToDecimal(txtProductPrice.Text),
+                CustomerName=txtCustomerName.Text
+            });
+            urunyukle();
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            _productdal.Delete(id);
+            urunyukle();
+        }
+    }
+}
---
+++ WpfApp1/Productdal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WpfApp1
+{
+ public  class Productdal
+    {
+        SqlConnection _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CarsCustomerDatabase;Integrated Security=True;");
+        public List<Product> _TakeProduct()
+        {
+            CheckConnection();
+            SqlCommand command = new SqlCommand("select * from CarsTable", _connection);
+            SqlDataReader Reader = command.ExecuteReader();
+            List<Product> products = new List<Product>();
+            while (Reader.Read())
+            {
+                Product product = new Product()
+                {
+                    
+                    CustomerId =Convert.ToInt32(Reader[0]),
+                    CustomerName = Reader[1].ToString(),
+                    Carsname = Reader[2].ToString(),
+                    CarsPrice = Convert.ToDecimal(Reader[3])
+                };
+                products.Add(product);
+            }
+            Reader.Close();
+            _connection.Close();
+            return products;
+        }
+        public void Add(Product product)
+        {
+            CheckConnection();
+            SqlCommand command = new SqlCommand("insert into CarsTable values(@cname,@carsname,@carsprice)", _connection);
+            command.Parameters.AddWithValue("@cname", product.CustomerName);
+            command.Parameters.AddWithValue("@carsname", product.Carsname);
+            command.Parameters.AddWithValue("@carsprice", product.CarsPrice);
+            command.ExecuteNonQuery();
+            _connection.Close();
+        }
+        public void Update(Product product)
+        {
+            CheckConnection();
+            SqlCommand command = new SqlCommand("Update CarsTable set CustomerName=@cname,Carsname=@carsname,CarsPrice=@carsprice where CustomerId=@ID", _connection);
+            command.Parameters.AddWithValue("@ID", product.CustomerId);
+            command.Parameters.AddWithValue("@cname", product.CustomerName);
+            command.Parameters.AddWithValue("@carsname", product.Carsname);
+            command.Parameters.AddWithValue("@carsprice", product.CarsPrice);
+            command.ExecuteNonQuery();
+            _connection.Close();
+        }
+        public void Delete(int id)
+        {
+            CheckConnection();
+            SqlCommand command = new SqlCommand("Delete from CarsTable  where CustomerId=@ID", _connection);
+            command.Parameters.AddWithValue("@ID",id);
+             
+            command.ExecuteNonQuery();
+            _connection.Close();
+        }
+        public void CheckConnection()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+    }
+}
+    
